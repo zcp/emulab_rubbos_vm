30a292ad709bf7f71d54b0ee5ff11a40a7004533
zcp <is99zcp@cqut.edu.cn>
2019-02-10 12:02:52 -0500

    a commit

--- git.sh
+++ git.sh
@@ -2,4 +2,4 @@
 git init
 git add .
 git commit -m "a commit"
-git push -u https://zcp:324zq999@github.com/zcp/emulab_rubbos.git
+git push -u https://zcp:324zq999@github.com/zcp/emulab_rubbos_vm.git
